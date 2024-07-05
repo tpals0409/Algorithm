f5ed7a4576053e6779e9dfe0515cc502997b3c8c
SeMin Kim <83855438+tpals0409@users.noreply.github.com>
2024-07-05 13:25:51 +0900

    [Silver V] Title: 줄세우기, Time: 148 ms, Memory: 110628 KB -BaekjoonHub

---
+++ 백준/Silver/10431. 줄세우기/줄세우기.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+testcase = int(input())
+
+for i in range(testcase):
+    students = list(map(int, input().split()))
+    number = students.pop(0)
+    sorting = [0]
+    answer = 0
+    for student in students:
+        start = 0
+        while True:
+            if student > sorting[start]:
+                sorting.insert(start, student)
+                answer += start
+                break
+            else:
+                start += 1
+    print(number, answer)
