dadc55d47a1a46d7bdcf26b5c9cc2d328332e9d0
SeMin Kim <83855438+tpals0409@users.noreply.github.com>
2024-07-08 13:11:37 +0900

    [Silver V] Title: 덩치, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.py
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -1,19 +1,13 @@
-import sys
-input = sys.stdin.readline
-
 N = int(input())
+people = []
+for _ in range(N):
+    person = list(map(int, input().split()))
+    people.append(person)
 
-score = [0]*N
 
 for i in range(N):
-    kg, cm = map(int, input().split())
-    score[i] = [kg, cm]
-
-for j in range(N):
-    tmp = score[j]
-    count = 1
-    for k in range(N):
-        if j != k:
-            if score[k][0] > tmp[0] and score[k][1] > tmp[1]:
-                count += 1
-    print(count, end=' ')
+    answer = 1
+    for j in range(N):
+        if (people[i][0] < people[j][0]) and (people[i][1] < people[j][1]):
+            answer += 1
+    print(answer, end = ' ')
