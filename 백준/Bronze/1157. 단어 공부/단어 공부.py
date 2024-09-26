54991fa4909a7bcb9eaa4b97db3d93b60f295153
SeMin Kim <83855438+tpals0409@users.noreply.github.com>
2024-09-26 10:25:58 +0900

    [Bronze I] Title: 단어 공부, Time: 180 ms, Memory: 33076 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,22 +1,21 @@
 word = input()
 word = word.upper()
-
-alphabet = set(word)
-count = [[alpha, 0] for alpha in alphabet]
-
-for alpha in word:
-    for i in range(len(count)):
-        if alpha in count[i]:
-            count[i][1] += 1
-
-count = sorted(count, key= lambda x : x[1], reverse = True)
-max_value = count[0][1]
-answer = count[0][0]
-for i in range(1, len(count)):
-    if count[i][1] == max_value:
-        answer = '?'
-        break
+count = dict()
+for i in word:
+    if i in count:
+        count[i] += 1
     else:
-        break
+        count[i] = 1
 
-print(answer)
+answer = []
+number = []
+for j in count:
+    answer.append([j, count[j]])
+    number.append(count[j])
+
+answer = sorted(answer, key= lambda x : x[1], reverse=True)
+
+if number.count(answer[0][1]) != 1:
+    print('?')
+else:
+    print(answer[0][0])
