455cd7472472c050a537f92af0bad7b83d2ccce5
SeMin Kim <83855438+tpals0409@users.noreply.github.com>
2024-10-03 16:17:38 +0900

    [Silver IV] Title: 수 찾기, Time: 652 ms, Memory: 47016 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,27 +1,22 @@
-pre_N = int(input())  # Number of elements in pre_list
-pre_list = list(map(int, input().split()))  # Elements in pre_list
-N = int(input())  # Number of elements in is_in
-is_in = list(map(int, input().split()))  # Elements to check for presence
+pre_N = int(input())
+pre_list = list(map(int, input().split()))
+N = int(input())
+is_in = list(map(int, input().split()))
 
-# Sort pre_list
 pre_list = sorted(pre_list)
-
-# Perform binary search for each element in is_in
 for i in is_in:
     start = 0
-    end = pre_N - 1  # Set end to pre_N - 1 because list indices are zero-based
-    
+    end = pre_N-1
     found = False
-    while start <= end:
-        mid = (start + end) // 2  # Recalculate the mid index
-        if pre_list[mid] == i:  # If the element is found
+    while (start<=end):
+        mid = (end+start)//2
+        if i == pre_list[mid]:
             found = True
             break
-        elif pre_list[mid] < i:  # If the element is greater than the mid, search the right half
-            start = mid + 1
-        else:  # If the element is less than the mid, search the left half
-            end = mid - 1
-
+        elif i > pre_list[mid]:
+            start = mid+1
+        elif i < pre_list[mid]:
+            end = mid-1
     if found:
         print(1)
     else:
