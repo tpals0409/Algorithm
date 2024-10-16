ba3ae4e919b25a382dbba738b3c39d4a882485b2
SeMin Kim <83855438+tpals0409@users.noreply.github.com>
2024-10-16 10:10:44 +0900

    [Silver III] Title: 타노스, Time: 40 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.py
+++ 백준/Silver/20310. 타노스/타노스.py
@@ -1,8 +1,26 @@
 zero_one = str(input())
-zero_count = zero_one.count('0')
-one_count = zero_one.count('1')
+zero_count = zero_one.count('0')//2
+one_count = zero_one.count('1')//2
 
-for i in range(zero_count//2):
-    print('0', end='')
-for i in range(one_count//2):
-    print('1', end='')
+tmp = ''
+for i in zero_one: #문자열에서 앞에서 부터 0을 채움
+    if i == '0':
+        if zero_count > 0:
+            tmp += i
+            zero_count -= 1
+    else:
+        tmp += i
+
+answer = ''
+tmp = list(reversed(tmp)) # 0을 제거한 문자열에서 뒤에서부터 1을 빼기 위해 reverse함
+for j in tmp:
+    if j == '1':
+        if one_count > 0:
+            answer += j
+            one_count -= 1
+    else:
+        answer += j
+answer = list(reversed(answer)) #다시 reverse해서 원래 순서로 바꿈
+
+for k in answer:
+    print(k, end='')
