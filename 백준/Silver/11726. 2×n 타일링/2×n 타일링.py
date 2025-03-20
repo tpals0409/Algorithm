13861b4245bb2dc1c297b110c9d30f44b3584224
SeMin Kim <83855438+tpals0409@users.noreply.github.com>
2025-03-20 15:08:43 +0900

    [Silver III] Title: 2×n 타일링, Time: 44 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+dp = [0 for _ in range(N)]
+if N>=1:
+    dp[0] = 1
+if N>=2:
+    dp[1] = 2
+
+for i in range(2, N):
+    dp[i] = dp[i-1]+dp[i-2]
+print(dp[-1]%10007)
