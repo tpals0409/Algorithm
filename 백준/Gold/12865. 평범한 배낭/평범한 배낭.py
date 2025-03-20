feec3a4b44736a6084fe0a4e06e98e3e81ec7088
SeMin Kim <83855438+tpals0409@users.noreply.github.com>
2025-03-20 14:42:51 +0900

    [Gold V] Title: 평범한 배낭, Time: 2540 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,13 +1,12 @@
 import sys
 input = sys.stdin.readline
-
 N, K = map(int, input().split())
 objects = [list(map(int, input().split())) for _ in range(N)]
+dp = [0 for _ in range(K+1)]
 
-dp = [0] * (K + 1)
+for i in range(N):
+    weight, value = objects[i]
+    for j in range(K, weight-1, -1):
+        dp[j] = max(dp[j], dp[j-weight]+value)
 
-for w, v in objects:
-    for j in range(K, w - 1, -1):
-        dp[j] = max(dp[j], dp[j - w] + v)
-
-print(dp[K])
+print(dp[-1])
