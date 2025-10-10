9a3cf199ec4b3879990aff9d07c4d1eaf12d5a93
SeMin Kim <83855438+tpals0409@users.noreply.github.com>
2025-10-10 13:46:49 +0900

    [level 2] Title: 타겟 넘버, Time: 355.85 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,9 +1,21 @@
+from collections import deque
 def solution(numbers, target):
-    answer = [[] for _ in range(len(numbers))]
-    answer[0] = [-numbers[0], numbers[0]]
-    for i in range(1, len(numbers)):
-        for j in range(len(answer[i-1])):
-            answer[i].append((answer[i-1][j]-numbers[i]))
-            answer[i].append((answer[i-1][j]+numbers[i]))
-    result = answer[-1].count(target)
-    return result
+    answer = 0
+    num = deque([0])
+    length = len(numbers)
+    numbers = deque(numbers)
+    while numbers:
+        n = numbers.popleft()
+        case = deque()
+        while num:
+            tmp = num.popleft()
+            case.append(tmp+n)
+            case.append(tmp-n)
+        num = case
+    
+    while case:
+        ans = case.popleft()
+        if ans == target:
+            answer += 1
+            
+    return answer
