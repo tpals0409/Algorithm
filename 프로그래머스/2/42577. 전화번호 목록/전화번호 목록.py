40e1f9df2c6d74e14dce2c68bb84b350f6b08a45
SeMin Kim <83855438+tpals0409@users.noreply.github.com>
2025-10-17 13:40:03 +0900

    [level 2] Title: 전화번호 목록, Time: 499.56 ms, Memory: 40.5 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,7 +1,9 @@
 def solution(phone_book):
-    hash_map = set(phone_book)
-    for phone in phone_book:
-        for i in range(1, len(phone)):
-            if phone[:i] in hash_map:
-                return False
-    return True
+    answer = True
+    standard = set(phone_book)
+    for i in range(len(phone_book)):
+        for j in range(1, len(phone_book[i])):
+            if phone_book[i][:j] in standard:
+                answer = False
+                break
+    return answer
