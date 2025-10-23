5a59a2b4d361ad6beccc4fac79b4ea532854916b
SeMin Kim <83855438+tpals0409@users.noreply.github.com>
2025-10-23 15:14:28 +0900

    [level 3] Title: 여행경로, Time: 0.04 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,14 +1,14 @@
-from collections import deque, defaultdict
+from collections import defaultdict
 def solution(tickets):
     answer = []
     graph = defaultdict(list)
     for start, end in sorted(tickets, reverse = True):
         graph[start].append(end)
     
-    def dfs(airport):
-        while graph[airport]:
-            next_airport = graph[airport].pop()
-            dfs(next_airport)
-        answer.append(airport)
+    def dfs(now):
+        while graph[now]:
+            nxt = graph[now].pop()
+            dfs(nxt)
+        answer.append(now)
     dfs('ICN')
     return answer[::-1]
