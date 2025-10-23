ea705ce376220ad1bb78cd424a4bc2c32915e5f7
SeMin Kim <83855438+tpals0409@users.noreply.github.com>
2025-10-23 15:07:41 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.58 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,22 +1,22 @@
 from collections import deque
 def solution(maps):
     answer = 0
-    n = len(maps)
-    m = len(maps[0])
-    visited = [[False for i in range(len(maps[0]))] for i in range(len(maps))]
-    q = deque([[0, 0, 1]])
+    len_y = len(maps)
+    len_x = len(maps[0])
+    
+    visited = [[False for i in range(len_x)] for i in range(len_y)]
     visited[0][0] = True
+    q = deque([[0, 0, 1]])
     while q:
-        y, x, dist = q.popleft()
         move = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-        
-        if y == n - 1 and x == m - 1:
-            return dist
-        else:
-            for dy, dx in move:
-                ny, nx = y+dy, x+dx
-                if (0<=ny<len(maps)) and (0<=nx<len(maps[0])):
-                    if maps[ny][nx] == 1 and visited[ny][nx] == False:
-                        visited[ny][nx] = True
-                        q.append([ny, nx, dist+1])
+        y, x, count = q.popleft()
+        if (y == len_y-1) and (x == len_x-1):
+            return count
+        for dy, dx in move:
+            ny, nx = y+dy, x+dx
+            if (0<=ny<len_y) and (0<=nx<len_x):
+                if not (visited[ny][nx]) and (maps[ny][nx] == 1):
+                    visited[ny][nx] = True
+                    q.append([ny, nx, count+1])
+            
     return -1
